8acda04457dd292a24a4086fb8b2dc12e2a12a5e
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-04-24 14:06:17 +0200

    Script for generating the zip files

--- codalab_competition_bundle/generate_zip_for_codalab.sh
+++ codalab_competition_bundle/generate_zip_for_codalab.sh
@@ -1,6 +1,7 @@
 for filename in AutoDL_*; do
   cd $filename;
   echo $filename;
-  zip -r --exclude=*__pycache__* --exclude=*.DS_Store* ../$filename .;
+  DATE=`date +%Y_%m_%d`;
+  zip -r --exclude=*__pycache__* --exclude=*.DS_Store* "../$filename-$DATE" .;
   cd ..;
 done
