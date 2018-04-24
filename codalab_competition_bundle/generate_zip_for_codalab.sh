0cc2108c3f2342b4e6b744908cb4f00623784a75
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-04-24 15:57:18 +0200

    Modify shell script

--- codalab_competition_bundle/generate_zip_for_codalab.sh
+++ codalab_competition_bundle/generate_zip_for_codalab.sh
@@ -1,16 +1,7 @@
-# for filename in AutoDL_*; do
-#   cd $filename;
-#   echo $filename;
-#   DATE=`date +%Y_%m_%d`;
-#   zip -r --exclude=*__pycache__* --exclude=*.DS_Store* "../$filename-$DATE" .;
-#   cd ..;
-# done
-
-INPUT_DIR="AutoDL_input_data_1/"
-cd $INPUT_DIR
-for dataset in */; do
-  cd $dataset;
-  echo "Zipping $dataset"
-  zip -r --exclude=*__pycache__* --exclude=*.DS_Store* ../${dataset%%/*} .;
+for filename in AutoDL_*; do
+  cd $filename;
+  echo $filename;
+  DATE=`date +%Y_%m_%d`;
+  zip -r --exclude=*__pycache__* --exclude=*.DS_Store* "../$filename-$DATE" .;
   cd ..;
 done
