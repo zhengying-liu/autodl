412f489bce22847e26d1a77685f35638ded770eb
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-10-08 03:52:36 +0200

    Important change: ready for beta testing

--- codalab_competition_bundle/AutoDL_parent_scoring_program/evaluate.py
+++ codalab_competition_bundle/AutoDL_parent_scoring_program/evaluate.py
@@ -4,50 +4,56 @@ import os
 import os.path
 import time
 
+# Constant used for a missing score
+missing_score = -0.999999
+
 _start = time.time()
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 
-n_datasets = 5 # TODO: to be changed to 5
+# We have 5 datasets (tasks) in total
+n_datasets = 5
 
 # Parent phase has 1 as phase number by default
 
 submit_dirs = []
+score_names = []
 
 for phase_number in range(2, 2 + n_datasets):
   submit_dir = os.path.join(input_dir, 'res_' + str(phase_number))
   submit_dirs.append(submit_dir)
+  score_name = 'set{}_score'.format(phase_number - 1)
+  score_names.append(score_name)
 
-score = 0
+scores = []
 
 for submit_dir in submit_dirs:
-    if not os.path.isdir(submit_dir):
-        print("%s doesn't exist" % submit_dir)
-
     if os.path.isdir(submit_dir):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-
         submission_score_file = os.path.join(submit_dir, "scores.txt")
         submission_score = open(submission_score_file).readline()
-
         # Score is written like so: "score:<score>\nDuration:11.11"
         score_text = submission_score.split(":")[1]
-
-        score += float(score_text)
-
+        scores.append(float(score_text))
+    else:
+        print("{} doesn't exist. Use missing score.".format(submit_dir))
+        scores.append(missing_score)
 
 detailed_results_path = os.path.join(output_dir, "detailed_results.html")
 # Write to detailed results
 with open(detailed_results_path, 'a+') as detailed_results:
 #     detailed_results.write('<head> <meta http-equiv="refresh" content="1"> </head>')
-    detailed_results.write("Oh yeah! Now it's good with real-time output and parallel tracks!")
+    detailed_results.write("Oh yeah! Now AutoDL is ready for beta testing!")
 
 _end = time.time()
 _duration = _start - _end
 
 output_filename = os.path.join(output_dir, 'scores.txt')
 with open(output_filename, 'w') as output_file:
-	output_file.write("score:{}\n".format(score))
-	output_file.write("Duration:{0:.6f}\n".format(_duration))
+  for i in range(n_datasets):
+    score_name = score_names[i]
+    score = scores[i]
+    output_file.write("{}: {}\n".format(score_name, score))
+  output_file.write("Duration: {:.6f}\n".format(_duration))
---
+++ codalab_competition_bundle/utilities/clear_last_output.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script clears possible output of last local execution
+
+ROOT_DIR=$(pwd)
+STARTING_KIT_DIR=$ROOT_DIR/../AutoDL_starting_kit/
+cd $STARTING_KIT_DIR
+rm -rf AutoDL_scoring_output
+rm -rf AutoDL_sample_result_submission
+ls | grep checkpoints_* | xargs rm -rf
