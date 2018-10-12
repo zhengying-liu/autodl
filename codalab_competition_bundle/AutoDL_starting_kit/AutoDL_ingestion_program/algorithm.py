c40e33d12a20dadefad93ddcb2d78484ded0a3cf
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-10-12 23:13:22 +0200

    Fix doc

--- codalab_competition_bundle/AutoDL_starting_kit/AutoDL_ingestion_program/algorithm.py
+++ codalab_competition_bundle/AutoDL_starting_kit/AutoDL_ingestion_program/algorithm.py
@@ -56,6 +56,7 @@ class Algorithm(object):
     Returns:
       predictions: A `numpy.ndarray` matrix of shape (sample_count, output_dim).
           here `sample_count` is the number of examples in this dataset as test
-          set and `output_dim` is the number of labels to be predicted.
+          set and `output_dim` is the number of labels to be predicted. The
+          values should be binary or in the interval [0,1].
     """
     raise NotImplementedError("Algorithm class does not have any testing.")
