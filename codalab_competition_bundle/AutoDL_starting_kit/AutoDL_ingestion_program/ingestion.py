719837e85a8876f89ad0e01d8fa90d5c4e29a21c
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-10-18 00:11:37 +0200

    Remove 'repeat' feature in AutoDLDataset and let participant decide

--- codalab_competition_bundle/AutoDL_starting_kit/AutoDL_ingestion_program/ingestion.py
+++ codalab_competition_bundle/AutoDL_starting_kit/AutoDL_ingestion_program/ingestion.py
@@ -273,12 +273,8 @@ if __name__=="__main__" and debug_mode<4:
         print_log("Reading training set and test set...")
 
         ##### Begin creating training set and test set #####
-        ## default batch size for training set is 30
-        ## for test test is 1000
         D_train = AutoDLDataset(os.path.join(input_dir, basename, "train"))
-        D_test = AutoDLDataset(os.path.join(input_dir, basename, "test"), repeat=False)
-        # D_train.init(batch_size=30, repeat=True)
-        # D_test.init(batch_size=1000, repeat=False)
+        D_test = AutoDLDataset(os.path.join(input_dir, basename, "test"))
         ##### End creating training set and test set #####
 
         # ======== Keep track of time
