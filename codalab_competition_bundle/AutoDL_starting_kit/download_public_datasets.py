0be159096515dd6f61f248d0f7d34f2de4d1943f
Adrien Pavao <adrien.pavao@gmail.com>
2019-04-11 01:01:18 +0200

    Add Hammer dataset URLs.

--- codalab_competition_bundle/AutoDL_starting_kit/download_public_datasets.py
+++ codalab_competition_bundle/AutoDL_starting_kit/download_public_datasets.py
@@ -16,14 +16,14 @@ def main(*argv):
       'Chucky':'https://autodl.lri.fr/my/datasets/download/d06aa5fc-1fb5-4283-8e05-abed4ccdd975',
       'Pedro':'https://autodl.lri.fr/my/datasets/download/61a074cd-e909-4d49-b313-7da0d4f7dc8b',
       'Decal':'https://autodl.lri.fr/my/datasets/download/dfd93c39-e0d4-41b2-b332-4dd002676e05',
-      'Hammer':''
+      'Hammer':'https://autodl.lri.fr/my/datasets/download/eb569948-72f0-4002-8e4d-479a27766cbf'
   }
   solution_urls = {
       'Munster':'https://autodl.lri.fr/my/datasets/download/f3a61a40-b1f1-4ded-bc55-fb730a12f4c4',
       'Chucky':'https://autodl.lri.fr/my/datasets/download/29932707-21cc-4670-a7db-cdc246a8ab71',
       'Pedro':'https://autodl.lri.fr/my/datasets/download/852c1e68-5e91-477e-bef0-824b503814e8',
       'Decal':'https://autodl.lri.fr/my/datasets/download/d72cba79-3051-4779-b624-e50335aad874',
-      'Hammer':'https://autodl.lri.fr/my/datasets/download/e5a392c6-3bd1-4acf-8dcc-77bf89448616'
+      'Hammer':'https://autodl.lri.fr/my/datasets/download/c3729c98-4755-47a2-b764-a4159c5ca152'
   }
 
   def _HERE(*args):
