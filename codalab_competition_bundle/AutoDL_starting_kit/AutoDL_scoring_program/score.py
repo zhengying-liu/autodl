916222ac3bc42cfeae889c09bf9afad580e9f58d
Zhengying Liu <zhengying.liu@polytechnique.edu>
2019-09-08 17:52:56 +0200

    Update version and description; print version at beginning

--- codalab_competition_bundle/AutoDL_starting_kit/AutoDL_scoring_program/score.py
+++ codalab_competition_bundle/AutoDL_starting_kit/AutoDL_scoring_program/score.py
@@ -7,12 +7,13 @@
 #           prediction_dir should contain e.g. start.txt, adult.predict_0, adult.predict_1,..., end.txt.
 #           score_dir should contain scores.txt, detailed_results.html
 
-VERSION = 'v20190823'
+VERSION = 'v20190908'
 DESCRIPTION =\
 """This is the scoring program for AutoDL challenge. It takes the predictions
 made by ingestion program as input and compare to the solution file and produce
 a learning curve.
 Previous updates:
+20190908: [ZY] Add algebraic operations of learning curves
 20190823: [ZY] Fix the ALC in learning curve: use auc_step instead of auc
 20190820: [ZY] Minor fix: change wait time (for ingestion) from 30s to 90s
 20190709: [ZY] Resolve all issues; rearrange some logging messages;
@@ -941,6 +942,8 @@ class Evaluator(object):
 # =============================== MAIN ========================================
 
 if __name__ == "__main__":
+    logger.info("="*5 + " Start scoring program. " +
+                "Version: {} ".format(VERSION) + "="*5)
 
     # Default I/O directories:
     root_dir = _HERE(os.pardir)
